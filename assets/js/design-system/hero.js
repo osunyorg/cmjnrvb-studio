e44576d3e5bf937e75c45d0dc63e942aa97cdc32
Olivia206 <olivia.simonet206@gmail.com>
2025-08-29 23:39:46 +0200

    projects single

--- assets/js/design-system/hero.js
+++ assets/js/design-system/hero.js
@@ -3,33 +3,50 @@ import { isMobile } from '../theme/utils/breakpoints';
 class PagesHero {
     constructor () {
         this.page = document.querySelector('.projects__page');
-        if (!this.page || isMobile()) {
-            return;
+        this.home = document.querySelector('.page__home');
+
+        if ((this.page || this.home) && !isMobile()) {
+            this.listen();
         }
-        this.hero = this.page.querySelector('.hero')
-        this.listen();
     }
-
+    
     listen () {
-        this.heading = this.hero.querySelector('h1');
-        this.hgroup = this.hero.querySelector('hgroup');
-        this.date = this.hero.querySelector('.project-infos');
-        this.content = this.hero.querySelector('.content');
+        this.hero = document.querySelector('.hero');
+        this.title = this.hero.querySelector('h1');
+        this.heading = this.hero.querySelector('hgroup');
 
-        this.moveHeading();
-
-        if (this.hgroup) {
-            this.removeHgroupTag();
+        if (this.page) {
+            this.moveHeading();
+        }
+        if (this.home) {
+            this.moveButton();
         }
     }
 
     moveHeading() {
-        this.content.prepend(this.heading);
+        this.date = this.hero.querySelector('.project-infos');
+        this.content = this.hero.querySelector('.content');
+
+        this.content.prepend(this.title);
         this.content.prepend(this.date);
+
+        if (this.heading) {
+            this.removeParentTag(this.heading);
+        }
     }
 
-    removeHgroupTag() {
-        this.hgroup.replaceWith(...this.hgroup.childNodes);
+    moveButton() {
+        this.button = this.hero.querySelector('.hero-actions');
+        this.lead = this.heading.querySelector('p:last-of-type');
+
+        if (this.button) {
+            this.lead.append(this.button);
+            this.removeParentTag(this.button);
+        }
+    }
+
+    removeParentTag(element) {
+        element.replaceWith(...element.childNodes);
     }
 }
 
