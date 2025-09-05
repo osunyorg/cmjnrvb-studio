8a3c0ec74ca25532644345863de77e9c94a2d4eb
Olivia206 <olivia.simonet206@gmail.com>
2025-09-05 13:18:12 +0200

    use home block for logo hover

--- assets/js/design-system/header.js
+++ assets/js/design-system/header.js
@@ -1,32 +1,30 @@
-class Header {
+class FooterImages {
     constructor () {
-        this.coverContainer = document.querySelector('.header-cover');
-        if (!this.coverContainer) {
+        this.imagesContainer = document.querySelector('.header-cover');
+        if (!this.imagesContainer) {
             return;
         }
+        this.images = this.imagesContainer.querySelectorAll('picture');
+        this.total = this.images.length;
         this.listen();
     }
 
     listen () {
-        this.images = [];
-
-        this.getImagesUrl();
-        this.createImage();
+        this.hideImages();
+        this.displayImage();
     }
 
-    getImagesUrl() {
-        let imageIndex = Math.floor(Math.random() * 3) + 1
-        let imageUrl = `/assets/images/hover/hover-${imageIndex}.svg`;
-        this.images.push(imageUrl);
+    hideImages() {
+        for (var i = 0, len = this.images.length; i < len; i++) {
+            this.images[i].style.display = 'none';
+        }
     }
 
-    createImage () {
-        let image = document.createElement('img');
-        let url = this.images;
-
-        image.src = url;
-        this.coverContainer.appendChild(image);
+    displayImage () {
+        let image = this.images[Math.floor(Math.random() * this.total)];
+        console.log(Math.floor(Math.random() * this.total))
+        image.style.display = 'block';
     }
 }
 
-export default new Header();
+export default new FooterImages();
