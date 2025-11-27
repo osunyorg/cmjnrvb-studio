32fe6d98abac417cec603e0fbd20f8b056ea0322
Alexis BENOIT <alex@noesya.coop>
2025-11-27 09:02:54 +0100

    scroll

--- assets/js/design-system/imageLoadState.js
+++ assets/js/design-system/imageLoadState.js
@@ -1,12 +1,10 @@
 class ImageLoadState {
     constructor (img) {
         this.element = img;
-        console.log(this.element)
         if (this.element.complete) this.setClass();
         this.element.addEventListener('load', this.setClass.bind(this));
     }
     setClass() {
-        console.log('load')
         this.element.classList.add('loaded');
     }
 };
